ea3b51a6233365bf39d029f14c2f028cd29ccd93
imkrishn <963gurudev@gmail.com>
2026-02-27 16:49:15 +0530

    feat(backend): implement pipeline parse endpoint with DAG validation

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,67 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+from collections import defaultdict, deque
 
 app = FastAPI()
+
+# CORS access of frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Node(BaseModel):
+    id: str
+
+class Edge(BaseModel):
+    source: str
+    target: str
+
+class PipelineRequest(BaseModel):
+    nodes: List[Node]
+    edges: List[Edge]
+
+    #dag checker function
+
+def is_dag(nodes, edges):
+    graph = defaultdict(list)
+    indegree = defaultdict(int)
+
+    for node in nodes:
+        indegree[node.id] = 0
+
+    for edge in edges:
+        graph[edge.source].append(edge.target)
+        indegree[edge.target] += 1
+
+    queue = deque([n for n in indegree if indegree[n] == 0])
+    visited = 0
+
+    while queue:
+        current = queue.popleft()
+        visited += 1
+
+        for neighbor in graph[current]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                queue.append(neighbor)
+
+    return visited == len(nodes)
+
 
 @app.get('/')
 def read_root():
     return {'Ping': 'Pong'}
+    
+@app.post("/pipelines/parse")
+def parse_pipeline(pipeline: PipelineRequest):
+    return {
+        "num_nodes": len(pipeline.nodes),
+        "num_edges": len(pipeline.edges),
+        "is_dag": is_dag(pipeline.nodes, pipeline.edges),
+    }
 
-@app.get('/pipelines/parse')
-def parse_pipeline(pipeline: str = Form(...)):
-    return {'status': 'parsed'}
